9fab13c7f52cbe5e85175dac9d3a228266a81b0a
StephanNazarkin <stepan.nazarkin@ukr.net>
2022-05-14 15:23:12 +0300

    Services minor fix

--- Services/AccountService.py
+++ Services/AccountService.py
@@ -1,103 +1,82 @@
-import http
-import json.decoder
-
-import requests
 from constants import *
 from Exceptions.ResponseIsFalseException import ResponseIsFalseException
-from Exceptions.IncorrectPasswordException import IncorrectPasswordException
 
 
 class AccountService(object):
     def __init__(self):
-        self.request = "empty"
+        self.response = ""
         self.url = REQUEST_URL
-        self.methodName = "empty"
+        self.methodName = ""
 
     def register_user(self, user_name, email, password):
         self.methodName = "Account/Register"
         self.url += self.methodName
-        self.request = requests.post(self.url, json={'userName': user_name,
+        self.response = requests.post(self.url, json={'userName': user_name,
                                                      'email': email,
                                                      'password': password})
-        response_json = self.request.json()
-        response = {'status code': self.request.status_code,
-                    'user id': response_json['id'],
-                    'user name': response_json['userName'],
-                    'user email': response_json['email'],
-                    'user token': response_json['token']}
-        print(response)
-        return response
+        return self.response
 
     def login_user(self, user_name, password):
         self.methodName = "Account/Login"
         self.url += self.methodName
-        self.request = requests.post(self.url, json={'userName': user_name,
+        self.response = requests.post(self.url, json={'userName': user_name,
                                                      'password': password})
-        response_json = self.request.json()
-        print(response_json)
-        return response_json
+        return self.response
 
     def change_user_password(self, new_password, old_password):
         self.methodName = "Account/ChangePassword"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json={'id': USER_ID,
+        self.response = requests.post(self.url, headers=HEADERS, json={'id': USER_ID,
                                                                       'oldPassword': old_password,
                                                                       'newPassword': new_password})
-        response = {'status code': self.request.status_code,
-                    'response': self.request.text}
-        print(self.request.text)
-        return response
+        return self.response
 
     def get_user_by_username(self, username):
         self.methodName = "Account/GetUserByUserName"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=username)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=username)
+        return self.response
 
     def add_friend(self, friend_id):
         self.methodName = "Account/AddFriend"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=friend_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=friend_id)
+        return self.response
 
     def delete_friend(self, friend_id):
         self.methodName = "Account/DeleteFriend"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=friend_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=friend_id)
+        return self.response
 
     def get_all_friends(self, user_id):
         self.methodName = "Account/GetAllFriends"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=user_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=user_id)
+        return self.response
 
     def block_user(self, user_id):
         self.methodName = "Account/BlockUser"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=user_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=user_id)
+        return self.response
 
     def unblock_user(self, user_id):
         self.methodName = "Account/UnblockUser"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=user_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=user_id)
+        return self.response
 
     def get_all_blocked_users(self, user_id):
         self.methodName = "Account/GetAllBlockedUsers"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=user_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=user_id)
+        return self.response
 
     def update_user(self, user_name, email):
         self.methodName = "Account/UpdateUser"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json={'id': USER_ID,
+        self.response = requests.post(self.url, headers=HEADERS, json={'id': USER_ID,
                                                                       'userName': user_name,
                                                                       'email': email})
-        response = {'status code': self.request.status_code,
-                    'response': self.request.text}
-        if response['response'] == 'false':
-            raise ResponseIsFalseException()
-        return response
+        return self.response
--- Services/ChatroomService.py
+++ Services/ChatroomService.py
@@ -1,107 +1,102 @@
 import requests
 from constants import *
 
+
 class ChatroomService(object):
     def __init__(self):
-        self.request = "empty"
+        self.response = ""
         self.url = REQUEST_URL
-        self.methodName = "empty"
+        self.methodName = ""
 
     def create_chatroom(self, topic, user_id):
         self.methodName = "Chatroom/CreateChatroom"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json={'topic': topic,
+        self.response = requests.post(self.url, headers=HEADERS, json={'topic': topic,
                                                                       'userId': user_id})
-        return self.request
+        return self.response
 
     def edit_chatroom(self, chat_id, topic, password):
         self.methodName = "Chatroom/EditChatroom"
         self.url += self.methodName
-        self.request = requests.post(self.url, json={'chatId': chat_id,
+        self.response = requests.post(self.url, json={'chatId': chat_id,
                                                      'topic': topic,
                                                      'password': password})
-        return self.request
+        return self.response
 
     def delete_chatroom(self, chat_id):
         self.methodName = "Chatroom/DeleteChatroom?chatId={chatId}".format(chatId=chat_id)
         self.url += self.methodName
-        print(self.url)
-        self.request = requests.delete(self.url)
-        print(self.request)
-        return self.request
+        self.response = requests.delete(self.url)
+        return self.response
 
     def get_chatroom(self, chat_id):
         self.methodName = "Chatroom/GetChatroom?chatId={chatId}".format(chatId=chat_id)
         self.url += self.methodName
-        print(self.url)
-        self.request = requests.get(self.url)
-        print(self.request)
-        return self.request
+        self.response = requests.get(self.url)
+        return self.response
 
     def get_all_chatrooms(self):
         self.methodName = "Chatroom/GetAllChatrooms"
         self.url += self.methodName
-        self.request = requests.get(self.url, headers=HEADERS)
-        return self.request
+        self.response = requests.get(self.url, headers=HEADERS)
+        return self.response
 
     def add_to_chatroom(self, chat_id, user_id):
         self.methodName = "Chatroom/AddToChatroom?chatId={chatid}".format(chatid=chat_id)
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=user_id)
-        print(self.request.text)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=user_id)
+        return self.response
 
     def leave_from_chatroom(self, chat_id):
         self.methodName = "Chatroom/LeaveFromChatroom"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=chat_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=chat_id)
+        return self.response
 
     def kick_user(self, user_id):
         self.methodName = "Chatroom/KickUser"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=user_id)
-        print(self.request)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=user_id)
+        return self.response
 
     def ban_user(self, user_account_id):
         self.methodName = "Chatroom/BanUser"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=user_account_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=user_account_id)
+        return self.response
 
     def unban_user(self, user_account_id):
         self.methodName = "Chatroom/UnbanUser"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=user_account_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=user_account_id)
+        return self.response
 
     def set_admin(self, user_account_id):
         self.methodName = "Chatroom/SetAdmin"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=user_account_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=user_account_id)
+        return self.response
 
     def unset_admin(self, user_account_id):
         self.methodName = "Chatroom/UnsetAdmin"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=user_account_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=user_account_id)
+        return self.response
 
     def get_all_banned_users(self, chat_id):
         self.methodName = "Chatroom/GetAllBannedUsers"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=chat_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=chat_id)
+        return self.response
 
     def get_all_admins(self, chat_id):
         self.methodName = "Chatroom/GetAllAdmins"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=chat_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=chat_id)
+        return self.response
 
     def get_all_users_from_chat(self, chat_id):
         self.methodName = "Chatroom/GetAllUsers"
         self.url += self.methodName
-        self.request = requests.post(self.url, headers=HEADERS, json=chat_id)
-        return self.request
+        self.response = requests.post(self.url, headers=HEADERS, json=chat_id)
+        return self.response
